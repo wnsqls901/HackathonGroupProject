a6b0ace7baa818b21ba81af8133838fd8853e48d
Corey Thornton <cthornt8@my.westga.edu>
2018-04-07 15:10:41 -0400

    Add code to javascript file to move cirlce up

--- CODEFEST18-DAWOLVES-master/CODEFEST18-DAWOLVES/scripts/scripting.js
+++ CODEFEST18-DAWOLVES-master/CODEFEST18-DAWOLVES/scripts/scripting.js
@@ -1,22 +1,43 @@
 window.onload = function() {
 
+ document.getElementById("up").addEventListener("click", up);
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
+
 
  var x = canvas.width/15;
  var y = canvas.height-30;
 
+ var dx = 30;
+ var dy = -30;
+
 function draw(){
+
   ctx.beginPath();
  ctx.arc(x, y, 10, 0, Math.PI*2);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
+
+
+}
+
+
+function up(){
+
+  y += dy;
+  draw();
 }
 
 
 
 
-setInterval(draw,10);
+
+
+
+draw();
+
+
+
 
 }
