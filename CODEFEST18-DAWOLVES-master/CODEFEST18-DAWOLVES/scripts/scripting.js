bc59aab16cd146604d61c7e0ab5b0aeaa2cb0796
Corey Thornton <cthornt8@my.westga.edu>
2018-04-07 13:17:18 -0400

    moved placeholder character

--- CODEFEST18-DAWOLVES-master/CODEFEST18-DAWOLVES/scripts/scripting.js
+++ CODEFEST18-DAWOLVES-master/CODEFEST18-DAWOLVES/scripts/scripting.js
@@ -1,11 +1,22 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+window.onload = function() {
+
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+
+ var x = canvas.width/15;
+ var y = canvas.height-30;
 
 function draw(){
   ctx.beginPath();
- ctx.arc(50, 50, 10, 0, Math.PI*2);
+ ctx.arc(x, y, 10, 0, Math.PI*2);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
 }
+
+
+
+
 setInterval(draw,10);
+
+}
