16c6647573766cb9a1c58ba3f9853e2a55bc0696
Caleb Farara <cfarara1@my.westga.edu>
2018-04-07 15:39:56 -0400

    Merge.

--- CODEFEST18-DAWOLVES-master/CODEFEST18-DAWOLVES/scripts/scripting.js
+++ CODEFEST18-DAWOLVES-master/CODEFEST18-DAWOLVES/scripts/scripting.js
@@ -1,9 +1,12 @@
 window.onload = function() {
 
- document.getElementById("up").addEventListener("click", up);
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
 
+ var canvas = document.getElementById("myCanvas");
+ var ctx = canvas.getContext("2d");
+ document.getElementById("up").addEventListener("click", up);
+ document.getElementById("down").addEventListener("click", down);
+ document.getElementById("left").addEventListener("click", left);
+ document.getElementById("right").addEventListener("click", right);
 
  var x = canvas.width/15;
  var y = canvas.height-30;
@@ -14,10 +17,10 @@ window.onload = function() {
 function draw(){
 
   ctx.beginPath();
- ctx.arc(x, y, 10, 0, Math.PI*2);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
 
 
 }
@@ -28,11 +31,20 @@ function up(){
   draw();
 }
 
+function down(){
+  y -= dy;
+  draw();
+}
 
+function left(){
+ x -= dx;
+ draw();
+}
 
-
-
-
+function right(){
+  x += dx;
+  draw();
+}
 
 
 
